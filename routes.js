4c5bc1a190ba9ab1c0ab189f5373376bf3ac122b
Daniel Esteves <estevesd8@gmail.com>
2019-08-21 13:35:28 -0400

    only next without a server

--- routes.js
+++
@@ -1,8 +0,0 @@
-const routes = require('next-routes')
-
-module.exports = routes()
-  .add('index')
-  .add('sobre', '', 'about')
-  .add('certificaciones', '', 'certifications')
-// Here you can new routes
-// Read more in next-routes DOCS
