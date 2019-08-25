12e4240caa4d134c6a21efcc512478cd853871cb
Daniel Esteves <estevesd8@gmail.com>
2019-08-25 12:45:50 -0400

    updating workbox options

--- next.config.js
+++ next.config.js
@@ -21,7 +21,24 @@ module.exports = withPlugins([
 ], {
   target: 'serverless',
   workboxOpts: {
-    swDest: 'static/service-worker.js'
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
   },
   webpack (config) {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env))
