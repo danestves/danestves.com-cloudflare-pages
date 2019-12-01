c9db5612ae8b7987d1fa693ba025550659f50393
Daniel Esteves <imacpc@iMac-de-iMac.local>
2019-12-01 16:02:43 -0400

    blog page styles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,60 @@
 import React from "react"
-import Layout from "../components/layout"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default () => (
+import { Fade } from "../components"
+import Layout from "../components/layout"
+import removeMarkdown from "../helpers/removeMarkdown"
+
+export default ({ data }) => (
   <Layout>
-    <h1>Blog</h1>
+    {console.log(data.allStrapiBlogs)}
+    {data
+      ? data.allStrapiBlogs.nodes.map(item => (
+          <Fade key={item.id}>
+            <Link
+              to={`blog/${item.slug}`}
+              className="flex flex-wrap items-stretch my-5 overflow-hidden bg-white border rounded-lg shadow hover:shadow-lg transition-all transition-250"
+            >
+              <Img
+                fluid={item.cover.childImageSharp.fluid}
+                className="w-full sm:w-2/5 lg:w-1/3 xl:w-1/5 transition-all transition-250"
+              />
+
+              <div className="w-full sm:w-3/5 px-4 py-5 lg:w-2/3 xl:w-4/5 transition-all transition-250">
+                <div className="flex flex-col justify-around h-full">
+                  <h2 className="my-2 text-2xl leading-none text-center lg:text-3xl xl:text-left">
+                    {item.title}
+                  </h2>
+
+                  <p className="mt-4 text-gray-500 lg:text-2xl font-light xl:text-xl">
+                    {removeMarkdown(item.content.substr(0, 154))}...
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </Fade>
+        ))
+      : ""}
   </Layout>
 )
+
+export const query = graphql`
+  query Blogs {
+    allStrapiBlogs(sort: { fields: [date], order: [DESC] }) {
+      nodes {
+        id
+        slug
+        cover {
+          childImageSharp {
+            fluid(maxWidth: 300, maxHeight: 200) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        title
+        content
+      }
+    }
+  }
+`
--- src/pages/portafolio.js
+++ src/pages/portafolio.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
+
 import Layout from "../components/layout"
 import { Fade } from "../components"
 
 export default ({ data }) => (
   <Layout>
-    {console.log(data.allStrapiPortfolios)}
     <div className="flex flex-wrap justify-center py-5">
       {data
         ? data.allStrapiPortfolios.nodes.map(item => (
             <Link
               to={`portafolio/${item.slug}`}
+              key={item.id}
               className="w-full px-3 my-3 sm:w-1/2 md:w-1/3 lg:w-1/4 portfolio-link"
             >
               <Fade>
@@ -22,10 +23,10 @@ export default ({ data }) => (
 
                 <div className="relative px-4 -mt-10">
                   <div className="flex flex-col bg-white border rounded-lg portfolio-meta transition-all transition-250">
-                    <h2 className="my-2 text-2xl leading-none lg:text-xl text-center">
+                    <h2 className="my-2 text-2xl leading-none text-center lg:text-xl">
                       {item.title}
                     </h2>
-                    <h3 className="mb-2 text-xl font-light leading-none text-gray-500 lg:text-lg text-center">
+                    <h3 className="mb-2 text-xl font-light leading-none text-center text-gray-500 lg:text-lg">
                       {item.category.name}
                     </h3>
                   </div>
@@ -39,7 +40,7 @@ export default ({ data }) => (
 )
 
 export const query = graphql`
-  query Portafolio {
+  query Portafolios {
     allStrapiPortfolios(sort: { fields: [title], order: [ASC] }) {
       nodes {
         id
