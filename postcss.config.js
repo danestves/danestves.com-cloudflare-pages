43116e87ce5e8b90c68508c908aaddd6eea25852
Daniel Esteves <estevesd8@gmail.com>
2019-12-08 23:42:41 -0400

    File size with purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "./src/pages/*.js",
+    "./src/components/*.js",
+    "./src/components/*.jsx",
+  ],
+  whitelist: ["html", "body"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+})
+
 module.exports = {
-	plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    require("cssnano"),
+  ],
 }
