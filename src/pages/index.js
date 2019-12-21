31a4944a35566f9b863d9876ea5f1659b642bd1f
Daniel Esteves <estevesd8@gmail.com>
2019-12-21 00:43:44 -0400

    fixing home design

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Services } from "../components"
-import { Coding, InterfaceIcon, DesignSystemIcon } from "../icons"
+import { InterfaceIcon, DesignSystemIcon } from "../icons"
 import Profile from "../images/profile.jpeg"
 
 const IndexPage = ({ data }) => (
@@ -24,20 +24,23 @@ const IndexPage = ({ data }) => (
           mucho más. Listo para realizar tu sueño.
         </p>
 
-        <div className="flex mt-5">
-          <Link
-            to="/curriculum"
-            className="w-1/2 px-8 py-2 mr-2 text-xl font-semibold text-center text-indigo-700 capitalize bg-transparent border border-indigo-700 rounded hover:bg-indigo-700 hover:text-white hover:border-transparent transition-colors transition-300 dark:text-gray-400 dark:border-gray-400 dark:hover:bg-gray-400 dark:hover:text-indigo-700"
-          >
-            curriculum
-          </Link>
-
-          <Link
-            to="/contacto"
-            className="w-1/2 px-8 py-2 ml-2 text-xl font-semibold text-center text-white capitalize bg-indigo-700 rounded hover:bg-indigo-600 transition-colors transition-300 transition-linear"
-          >
-            contactame
-          </Link>
+        <div className="flex flex-wrap mt-5">
+          <div className="w-full my-2 md:w-1/2 md:pr-2 md:my-0">
+            <Link
+              to="/curriculum"
+              className="block w-full px-8 py-2 text-xl font-semibold text-center text-indigo-700 capitalize bg-transparent border border-indigo-700 rounded hover:bg-indigo-700 hover:text-white hover:border-transparent transition-colors transition-250 dark:text-gray-400 dark:border-gray-400 dark:hover:bg-gray-400 dark:hover:text-indigo-700"
+            >
+              curriculum
+            </Link>
+          </div>
+          <div className="w-full my-2 md:w-1/2 md:pl-2 md:my-0">
+            <Link
+              to="/contacto"
+              className="block w-full px-8 py-2 text-xl font-semibold text-center text-white capitalize bg-indigo-700 rounded hover:bg-indigo-600 transition-colors transition-250"
+            >
+              contactame
+            </Link>
+          </div>
         </div>
       </div>
 
@@ -45,9 +48,7 @@ const IndexPage = ({ data }) => (
         <img
           src={Profile}
           alt="Daniel Esteves"
-          className="block object-cover object-center mx-auto rounded-full shadow-2xl"
-          width="400"
-          style={{ maxHeight: 400 }}
+          className="block object-cover object-center mx-auto rounded-full shadow-2xl transition-all transition-250 profile-photo"
         />
         {/* <Coding className="w-64 h-64 mx-auto drop-shadow" /> */}
       </div>
@@ -59,11 +60,11 @@ const IndexPage = ({ data }) => (
 
     <div className="flex flex-wrap items-center mb-10">
       <div className="order-2 w-full my-3 md:w-3/5 md:order-1">
-        <h3 className="mb-5 text-4xl font-medium leading-none text-indigo-800 dark:text-gray-300">
+        <h3 className="mb-5 text-4xl font-medium leading-none text-center text-indigo-800 md:text-left dark:text-gray-300">
           Interfaces limpias
         </h3>
 
-        <p className="mb-5 text-2xl text-gray-700 md:mb-0 dark:text-gray-500">
+        <p className="mb-5 text-2xl text-center text-gray-700 md:text-left md:mb-0 dark:text-gray-500">
           Al momento de realizar los prototipos de tus diseños se harán en base
           a estándares y accesibilidad con el cual tu sitio se verá muy limpio y
           rápido.
@@ -77,11 +78,11 @@ const IndexPage = ({ data }) => (
         <InterfaceIcon className="w-56 h-56 mx-auto mb-5 d-block md:mb-0" />
       </div>
       <div className="order-4 w-full my-3 md:w-3/5 md:order-3">
-        <h3 className="mb-5 text-4xl font-medium leading-none text-right text-indigo-800 dark:text-gray-300">
+        <h3 className="mb-5 text-4xl font-medium leading-none text-center text-indigo-800 md:text-right dark:text-gray-300">
           Adaptable a todos los dispositivos
         </h3>
 
-        <p className="mb-5 text-2xl text-right text-gray-700 md:mb-0 dark:text-gray-500">
+        <p className="mb-5 text-2xl text-center text-gray-700 md:text-right md:mb-0 dark:text-gray-500">
           Al momento de realizar los prototipos de tus diseños se harán en base
           a estándares de diseño y accesibilidad con el cual el diseño de tu
           sitio se verá muy limpio y rápido.
