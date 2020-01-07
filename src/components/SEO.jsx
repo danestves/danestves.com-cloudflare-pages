efc50b8eaf972e85b9b634bf93aeb697958ee4b0
Daniel Esteves <estevesd8@gmail.com>
2020-01-07 13:20:53 -0400

    summary large image for twitter

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -74,7 +74,7 @@ function SEO({ description, lang, meta, title, isTemplate }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
