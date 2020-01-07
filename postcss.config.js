a6e33c3eeb177eb49a0f0f7e16daa1ecbed9ea76
Daniel Esteves <estevesd8@gmail.com>
2020-01-06 20:10:29 -0400

    New config of postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,13 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.js", "./src/**/*.jsx"],
-  whitelist: ["html", "body"],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-})
-
 module.exports = {
   plugins: [
+    require("postcss-easy-import"),
     require("tailwindcss"),
+    process.env.NODE_ENV === "production" &&
+      require("@fullhuman/postcss-purgecss")({
+        content: ["./src/**/*.{js,jsx,ts,tsx}"],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+      }),
     require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
     require("cssnano"),
   ],
 }
