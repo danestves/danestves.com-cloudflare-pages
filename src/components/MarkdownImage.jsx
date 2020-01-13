0019d64d9c8b8962d79bd88adf8fc2a02f2efb6a
Daniel Esteves <estevesd8@gmail.com>
2020-01-13 03:54:23 +0000

    Fixing images lazy

--- src/components/MarkdownImage.jsx
+++ src/components/MarkdownImage.jsx
@@ -1,19 +1,26 @@
 import React, { useEffect } from "react"
+import { document } from "browser-monads"
 
 export default ({ alt, src }) => {
+  // useEffect(() => {
+  // if ("loading" in HTMLImageElement.prototype) {
+  //  const images = document.querySelectorAll('img[loading="lazy"]')
+  //  images.forEach(img => {
+  //    img.src = img.dataset.src
+  //  })
+  //} else {
+  //  // Dynamically import the LazySizes library
+  //  const script = document.createElement("script")
+  //  script.src =
+  //    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js"
+  //  document.body.appendChild(script)
+  //}
+  //}, [])
+
   useEffect(() => {
-    if ("loading" in HTMLImageElement.prototype) {
-      const images = document.querySelectorAll('img[loading="lazy"]')
-      images.forEach(img => {
-        img.src = img.dataset.src
-      })
-    } else {
-      // Dynamically import the LazySizes library
-      const script = document.createElement("script")
-      script.src =
-        "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js"
-      document.body.appendChild(script)
-    }
+    const script = document.createElement("script")
+    script.src= "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js"
+    document.body.appendChild(script)
   }, [])
 
   return <img src={src} loading="lazy" alt={`${alt} | Daniel Esteves`} />
