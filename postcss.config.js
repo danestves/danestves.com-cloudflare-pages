7d119ba252d1deefcde031917570909a6deeee7d
Daniel Esteves <estevesd8@gmail.com>
2020-01-19 00:10:19 -0400

    New extractor and postcss ignore

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
     process.env.NODE_ENV === "production" &&
       require("@fullhuman/postcss-purgecss")({
         content: ["./src/**/*.{js,jsx,ts,tsx}"],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
       }),
     require("autoprefixer"),
     require("cssnano"),
