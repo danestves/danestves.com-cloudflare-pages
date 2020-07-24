d1b09a2cf51c6c23915e58c26953e8d67a945ec4
Daniel Esteves <estevesd8@gmail.com>
2020-07-24 19:43:43 -0400

    feature: simple analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
       }
     },
     `gatsby-plugin-use-dark-mode`,
+    `gatsby-plugin-simple-analytics`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
