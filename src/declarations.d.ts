28fbcf6344c522ce253b1d2370cd60122ef356f3
Daniel Esteves <estevesd8@gmail.com>
2020-11-20 21:43:11 -0400

    feat: init

--- src/declarations.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'browser-monads';
-declare module 'remove-markdown';
-declare module 'emoji-trie';
-declare module '@glidejs/glide';
-declare module 'gatsby-plugin-disqus';
-declare module 'tailwindcss';
--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as usePlatziData } from './usePlatziData';
-export { default as useDocumentScrollThrottled } from './useDocumentScrollThrottled';
-export { default as useScrollPosition } from './useScrollPosition';
