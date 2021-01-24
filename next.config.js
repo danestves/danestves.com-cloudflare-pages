f1b878295a78882e589962e2f826d9d1fa0bdce6
Daniel Esteves <estevesd8@gmail.com>
2021-01-24 17:58:04 -0400

    feat: adding dest for pwa

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
   },
   images: {
     domains: ['media.graphcms.com', 'github-readme-stats.danestves.com'],
