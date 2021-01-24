a88f66cdb6509a57619a5798f95b6db73d077e5a
Daniel Esteves <estevesd8@gmail.com>
2021-01-24 18:11:49 -0400

    feat: source maps for production

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
+  productionBrowserSourceMaps: true,
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
