fbd59fc7887ac662b5c71b08b8f4faffa9a6f5ca
Daniel Esteves <estevesd8@gmail.com>
2021-01-24 19:33:45 -0400

    feat: setting bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 const withPWA = require('next-pwa')
-
-module.exports = withPWA({
-  productionBrowserSourceMaps: true,
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-  },
-  images: {
-    domains: ['media.graphcms.com', 'github-readme-stats.danestves.com'],
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    productionBrowserSourceMaps: true,
+    pwa: {
+      disable: process.env.NODE_ENV === 'development',
+      dest: 'public',
+    },
+    images: {
+      domains: ['media.graphcms.com', 'github-readme-stats.danestves.com'],
+    },
+  })
+)
