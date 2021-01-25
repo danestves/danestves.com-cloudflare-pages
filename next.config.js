5f8399d8b1dcf82f5fce62d7d08a1524ec3e98cc
Daniel Esteves <estevesd8@gmail.com>
2021-01-24 21:39:08 -0400

    feat: Replace React with Preact only in client production build

--- next.config.js
+++ next.config.js
@@ -22,5 +22,17 @@ module.exports = withPlugins(
     images: {
       domains: ['media.graphcms.com', 'github-readme-stats.danestves.com'],
     },
+    webpack: (config, { dev, isServer }) => {
+      // Replace React with Preact only in client production build
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          react: 'preact/compat',
+          'react-dom/test-utils': 'preact/test-utils',
+          'react-dom': 'preact/compat',
+        })
+      }
+
+      return config
+    },
   }
 )
