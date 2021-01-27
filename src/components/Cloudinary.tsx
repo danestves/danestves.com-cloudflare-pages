ff2142c4513ab15b208fa8cf1aa9bbed72ce5b7a
Daniel Esteves <estevesd8@gmail.com>
2021-01-27 16:38:34 -0400

    feat: adding portfolios

--- src/components/Cloudinary.tsx
+++ src/components/Cloudinary.tsx
@@ -1,6 +1,9 @@
 // Dependencies
 import * as React from 'react'
 
+// Components
+import { GeneralObserver } from '@/components'
+
 interface Props {
   publicID: string
   cloudName: string
@@ -57,19 +60,21 @@ const Cloudinary: React.FunctionComponent<Props> = ({
   const encodedSubtitle = subtitle ? encodeURIComponent(subtitle) : ''
 
   return (
-    <div className="embed-responsive aspect-ratio-16/9">
-      <iframe
-        title={title ? title : ''}
-        src={`https://player.cloudinary.com/embed/?public_id=${publicID}&cloud_name=${cloudName}&player%5Bfluid%5D=${fluid}&player%5Bcontrols%5D=${controls}&player%5Bcontrol_bar%5D%5Bfullscreen_toggle%5D=${fullscreen}&player%5Bshow_jump_controls%5D=${jumpControls}&player%5Bmuted%5D=${muted}&player%5Bcolors%5D%5Bbase%5D=${base}&player%5Bcolors%5D%5Baccent%5D=${accent}&player%5Bcolors%5D%5Btext%5D=${color}${logoOnClick}&${logoImage}${autoPlayMode}&player%5Bautoplay%5D=${autoPlay}&player%5Bloop%5D=${loop}&source%5Binfo%5D%5Btitle%5D=${encodedTitle}&source%5Binfo%5D%5Bsubtitle%5D=${encodedSubtitle}${
-          otherParams ? otherParams : ''
-        }`}
-        width="640"
-        height="480"
-        allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
-        allowFullScreen
-        frameBorder="0"
-      ></iframe>
-    </div>
+    <GeneralObserver>
+      <div className="embed-responsive aspect-ratio-16/9">
+        <iframe
+          title={title ? title : ''}
+          src={`https://player.cloudinary.com/embed/?public_id=${publicID}&cloud_name=${cloudName}&player%5Bfluid%5D=${fluid}&player%5Bcontrols%5D=${controls}&player%5Bcontrol_bar%5D%5Bfullscreen_toggle%5D=${fullscreen}&player%5Bshow_jump_controls%5D=${jumpControls}&player%5Bmuted%5D=${muted}&player%5Bcolors%5D%5Bbase%5D=${base}&player%5Bcolors%5D%5Baccent%5D=${accent}&player%5Bcolors%5D%5Btext%5D=${color}${logoOnClick}&${logoImage}${autoPlayMode}&player%5Bautoplay%5D=${autoPlay}&player%5Bloop%5D=${loop}&source%5Binfo%5D%5Btitle%5D=${encodedTitle}&source%5Binfo%5D%5Bsubtitle%5D=${encodedSubtitle}${
+            otherParams ? otherParams : ''
+          }`}
+          width="640"
+          height="480"
+          allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
+          allowFullScreen
+          frameBorder="0"
+        ></iframe>
+      </div>
+    </GeneralObserver>
   )
 }
 
---
+++ src/components/GeneralObserver.tsx
@@ -0,0 +1,48 @@
+// Dependencies
+import React, { FunctionComponent, useRef, useEffect, useState, RefObject } from 'react'
+
+interface IGeneralObserverProps {
+  /** Fires when IntersectionObserver enters viewport */
+  onEnter?: (id?: string) => void
+  /** The height of the placeholder div before the component renders in */
+  height?: number
+}
+
+const GeneralObserver: FunctionComponent<IGeneralObserverProps> = ({
+  children,
+  onEnter,
+  height = 0,
+}) => {
+  const ref = useRef<HTMLElement>(null)
+  const [isChildVisible, setIsChildVisible] = useState(false)
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.intersectionRatio > 0) {
+          setIsChildVisible(true)
+          onEnter && onEnter()
+        }
+      },
+      {
+        root: null,
+        rootMargin: '400px',
+        threshold: 0,
+      }
+    )
+    if (ref && ref.current) {
+      observer.observe(ref.current)
+    }
+  }, [ref])
+
+  return (
+    <div
+      ref={ref as RefObject<HTMLDivElement>}
+      data-testid="general-observer"
+      className="mdx-embed"
+    >
+      {isChildVisible ? children : <div style={{ height, width: '100%' }} />}
+    </div>
+  )
+}
+
+export default GeneralObserver
