baac475be3bb3ffc7c66632778bf9e0204338ddc
Daniel Esteves <estevesd8@gmail.com>
2021-01-29 02:39:13 -0400

    fix: Fixes npm packages that depend on  module

--- next.config.js
+++ next.config.js
@@ -34,6 +34,13 @@ module.exports = withPlugins(
         })
       }
 
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: 'empty',
+        }
+      }
+
       return config
     },
   }
