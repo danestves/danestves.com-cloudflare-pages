2450466c1503230bbcc97cfb68ba76f5211ed9d4
Daniel Esteves <estevesd8@gmail.com>
2021-01-29 03:02:37 -0400

    feat: removing no used next plugins

--- next.config.js
+++ next.config.js
@@ -1,47 +1,17 @@
-const withPlugins = require('next-compose-plugins')
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE,
-})
-const withPWA = require('next-pwa')
-const withMDX = require('@next/mdx')()
+module.exports = {
+  images: {
+    domains: ['github-readme-stats.danestves.com'],
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
 
-module.exports = withPlugins(
-  [
-    withBundleAnalyzer,
-    [
-      withPWA,
-      {
-        pwa: {
-          disable: process.env.NODE_ENV === 'development',
-          dest: 'public',
-        },
-      },
-    ],
-    withMDX
-  ],
-  {
-    productionBrowserSourceMaps: true,
-    images: {
-      domains: ['github-readme-stats.danestves.com'],
-    },
-    webpack: (config, { dev, isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.node = {
-          fs: 'empty',
-        }
-      }
-      
-      // Replace React with Preact only in client production build
-      if (!dev && !isServer) {
-        Object.assign(config.resolve.alias, {
-          react: 'preact/compat',
-          'react-dom/test-utils': 'preact/test-utils',
-          'react-dom': 'preact/compat',
-        })
-      }
-
-      return config
-    },
-  }
-)
+    return config
+  },
+}
