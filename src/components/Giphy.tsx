ab11981859de120b19d1db26c3c3b4efadc630a7
Daniel Esteves <estevesd8@gmail.com>
2021-02-04 00:09:44 -0400

    feat: adding intersection observer for gihpy

--- src/components/Giphy.tsx
+++ src/components/Giphy.tsx
@@ -3,6 +3,9 @@ import useSWR from 'swr'
 import { Gif } from '@giphy/react-components'
 import { IGif } from '@giphy/js-types'
 
+// Components
+import { GeneralObserver } from '@/components'
+
 // Libraries
 import fetcher from '@/lib/fetcher'
 
@@ -10,5 +13,9 @@ export default function Giphy({ id }: { id: string }): JSX.Element | null {
   const { data } = useSWR(`/api/giphy/${id}`, fetcher)
   const gif = ((data as unknown) as Record<string, unknown>)?.gif as IGif
 
-  return gif ? <Gif gif={gif} width={300} /> : null
+  return gif ? (
+    <GeneralObserver>
+      <Gif gif={gif} width={300} />
+    </GeneralObserver>
+  ) : null
 }
