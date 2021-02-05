b7838dbbfcb84ceb7e0cba3524b24f5c7b0fee59
Daniel Esteves <estevesd8@gmail.com>
2021-02-05 19:03:29 -0400

    fix: deleting unused we vitals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { AppProps } from 'next/app'
 import { LogoJsonLd, SocialProfileJsonLd } from 'next-seo'
 import { MDXProvider } from '@mdx-js/react'
 import { MDXEmbedProvider } from 'mdx-embed'
-import { window } from 'browser-monads'
 
 // Components
 import { Layout, SEO } from '@/components'
@@ -37,25 +36,6 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element | null {
       </MDXEmbedProvider>
     </MDXProvider>
   )
-}
-
-export function reportWebVitals({
-  id,
-  name,
-  label,
-  value,
-}: {
-  id: string
-  name: string
-  label: string
-  value: number
-}): void {
-  window.gtag('event', name, {
-    event_category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
-    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-    event_label: id, // id unique to current page load
-    non_interaction: true, // avoids affecting bounce rate.
-  })
 }
 
 export default MyApp
