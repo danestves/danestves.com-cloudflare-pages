a2093c664874764271e0ba42494498dc56ba5f17
Daniel Esteves <estevesd8@gmail.com>
2021-02-08 00:30:18 -0400

    feat: workbox-window

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,10 @@
 // Dependencies
-import { useState } from 'react'
 import { useRouter } from 'next/dist/client/router'
 
 // Components
-import { Header, Footer, CallToAction, Notification } from '@/components'
+import { Header, Footer, CallToAction } from '@/components'
 
 const Layout: React.FC = ({ children }) => {
-  const [isOpen, setIsOpen] = useState(false)
-
   const router = useRouter()
 
   return (
@@ -19,29 +16,6 @@ const Layout: React.FC = ({ children }) => {
       {router.pathname !== '/contacto' && <CallToAction />}
 
       <Footer />
-
-      <Notification
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        onClose={() => {
-          // eslint-disable-next-line
-          console.info(
-            'User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time.'
-          )
-        }}
-      >
-        <p className="flex-1 w-0 text-sm font-medium text-gray-900">
-          Hay una nueva versión del sitio 🎉
-        </p>
-
-        <button
-          type="button"
-          className="px-4 py-2 text-sm text-white rounded-lg bg-primary focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-white focus:outline-none"
-          // onClick={updateWorkbox}
-        >
-          Actualizar
-        </button>
-      </Notification>
     </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -59,6 +59,60 @@ export default class MyDocument extends Document {
         <body className="bg-secondary-700">
           <Main />
           <NextScript />
+
+          <script
+            type="module"
+            dangerouslySetInnerHTML={{
+              __html: `
+              import {Workbox, messageSW} from 'https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-window.prod.mjs';
+
+              if ('serviceWorker' in navigator) {
+                const wb = new Workbox('/sw.js');
+                let registration;
+
+                const showSkipWaitingPrompt = (event) => {
+                  // event.wasWaitingBeforeRegister will be false if this is
+                  // the first time the updated service worker is waiting.
+                  // When event.wasWaitingBeforeRegister is true, a previously
+                  // updated service worker is still waiting.
+                  // You may want to customize the UI prompt accordingly.
+
+                  // Assumes your app has some sort of prompt UI element
+                  // that a user can either accept or reject.
+                  const prompt = createUIPrompt({
+                    onAccept: async () => {
+                      // Assuming the user accepted the update, set up a listener
+                      // that will reload the page as soon as the previously waiting
+                      // service worker has taken control.
+                      wb.addEventListener('controlling', (event) => {
+                        window.location.reload();
+                      });
+
+                      if (registration && registration.waiting) {
+                        // Send a message to the waiting service worker,
+                        // instructing it to activate.  
+                        // Note: for this to work, you have to add a message
+                        // listener in your service worker. See below.
+                        messageSW(registration.waiting, {type: 'SKIP_WAITING'});
+                      }
+                    },
+
+                    onReject: () => {
+                      prompt.dismiss();
+                    }
+                  });
+                };
+
+                // Add an event listener to detect when the registered
+                // service worker has installed but is waiting to activate.
+                wb.addEventListener('waiting', showSkipWaitingPrompt);
+                wb.addEventListener('externalwaiting', showSkipWaitingPrompt);
+
+                wb.register().then((r) => registration = r);
+              }
+            `,
+            }}
+          />
         </body>
       </Html>
     )
