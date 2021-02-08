8b89fc6bb253a9d981328f9c0b8769e60d572097
Daniel Esteves <estevesd8@gmail.com>
2021-02-08 00:08:58 -0400

    fix: passing all options

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 const withPWA = require('next-pwa')
 
+// change start-url cache strategy, so that we can prompt user to reload when
+// new version available, instead of showing new version directly
+const runtimeCaching = require('next-pwa/cache')
+runtimeCaching[0].handler = 'StaleWhileRevalidate'
+
 module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-    register: true,
+    register: false,
+    skipWaiting: false,
+    runtimeCaching,
   },
   images: {
     domains: ['github-readme-stats.danestves.com'],
