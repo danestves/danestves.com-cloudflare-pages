a68da8983b85f3870e46663fae38319db61ed299
Daniel Esteves <estevesd8@gmail.com>
2021-02-07 23:54:49 -0400

    feat: using next-offline instead of next-pwa

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
-const withPWA = require('next-pwa')
+const withOffline = require('next-offline')
+const path = require('path')
 
-// change start-url cache strategy, so that we can prompt user to reload when
-// new version available, instead of showing new version directly
-const runtimeCaching = require('next-pwa/cache')
-runtimeCaching[0].handler = 'StaleWhileRevalidate'
-
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-    register: false,
-    skipWaiting: false,
-    runtimeCaching,
+module.exports = withOffline({
+  generateInDevMode: false,
+  dontAutoRegisterSw: true,
+  generateSw: false,
+  workboxOpts: {
+    swDest: path.join(__dirname, 'public', 'service-worker.js'),
+    swSrc: path.join(__dirname, 'public', 'sw.js'),
   },
   images: {
     domains: ['github-readme-stats.danestves.com'],
