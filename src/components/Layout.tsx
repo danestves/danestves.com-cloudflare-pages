848a0b0b45b9f42192de3132d124fbb0b9c86c4a
Daniel Esteves <estevesd8@gmail.com>
2021-02-07 22:24:52 -0400

    fix: updating app with native confirm

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,12 @@
 // Dependencies
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/dist/client/router'
 import { window } from 'browser-monads'
 
 // Components
-import { Header, Footer, CallToAction, Notification } from '@/components'
+import { Header, Footer, CallToAction } from '@/components'
 
 const Layout: React.FC = ({ children }) => {
-  const [confirm, setConfirm] = useState(false)
-  const [isOpen, setIsOpen] = useState(false)
-
   const router = useRouter()
 
   useEffect(() => {
@@ -24,12 +21,10 @@ const Layout: React.FC = ({ children }) => {
       // NOTE: MUST set skipWaiting to false in next.config.js pwa object
       // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
       const promptNewVersionAvailable = () => {
-        setIsOpen(true)
-
         // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
         // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
         // You may want to customize the UI prompt accordingly.
-        if (confirm) {
+        if (window.confirm('Hay una nueva versión del sitio 🎉')) {
           wb.addEventListener('controlling', () => {
             window.location.reload()
           })
@@ -50,7 +45,7 @@ const Layout: React.FC = ({ children }) => {
       // never forget to call register as auto register is turned off in next.config.js
       wb.register()
     }
-  }, [confirm])
+  }, [])
 
   return (
     <>
@@ -61,20 +56,6 @@ const Layout: React.FC = ({ children }) => {
       {router.pathname !== '/contacto' && <CallToAction />}
 
       <Footer />
-
-      <Notification isOpen={isOpen} setIsOpen={setIsOpen}>
-        <p className="flex-1 w-0 text-sm font-medium text-gray-900">
-          Hay una nueva versión del sitio 🎉
-        </p>
-
-        <button
-          type="button"
-          className="px-4 py-2 text-sm text-white rounded-lg bg-primary focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-white focus:outline-none"
-          onClick={() => setConfirm(true)}
-        >
-          Actualizar
-        </button>
-      </Notification>
     </>
   )
 }
