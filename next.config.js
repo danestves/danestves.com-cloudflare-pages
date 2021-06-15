7f24fac689da7a4786fe45be74a8fc727f6f00a4
Daniel Esteves <daniel@seeed.us>
2021-06-15 12:28:43 -0400

    fix(config): moving webpack to a top level

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const withProgressBar = require('next-progressbar')
 
 module.exports = withProgressBar(
   withPWA({
-    future: {
-      webpack5: true,
-    },
+    webpack5: true,
     pwa: {
       disable: process.env.NODE_ENV === 'development',
       dest: 'public',
