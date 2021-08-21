46855abade14393e39619d827e78eda01630c83b
Daniel Esteves <estevesd8@gmail.com>
2021-08-20 21:37:24 -0400

    feat: files order and tsconfig path

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, fireEvent } from '../testUtils'
-import { Home } from '../../pages/index'
+import { Home } from '@/pages'
 
 describe('Home page', () => {
   it('matches snapshot', () => {
