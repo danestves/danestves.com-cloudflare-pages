72a92eeb7e64f5898b283a5f303e3a23c0ea0f71
Daniel Esteves <estevesd8@gmail.com>
2021-08-23 01:36:13 -0400

    feat: pass ip from request and not ip from vercel

--- src/pages/api/country.ts
+++ src/pages/api/country.ts
@@ -1,37 +1,21 @@
 // Dependencies
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-function getIP(req: NextApiRequest) {
-  // req.connection is deprecated
-  const conRemoteAddress = req.connection?.remoteAddress
-  // req.socket is said to replace req.connection
-  const sockRemoteAddress = req.socket?.remoteAddress
-  // some platforms use x-real-ip
-  const xRealIP = req.headers['x-real-ip']
-  // most proxies use x-forwarded-for
-  const xForwardedForIP = (() => {
-    const xForwardedFor = req.headers['x-forwarded-for'] as string
-    if (xForwardedFor) {
-      // The x-forwarded-for header can contain a comma-separated list of
-      // IP's. Further, some are comma separated with spaces, so whitespace is trimmed.
-      const ips = xForwardedFor.split(',').map((ip) => ip.trim())
-      return ips[0]
-    }
-  })()
-  // prefer x-forwarded-for and fallback to the others
-  return xForwardedForIP || xRealIP || sockRemoteAddress || conRemoteAddress
-}
+// Internals
+import { getIP } from '@/lib'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
   try {
-    const country = await fetch('https://extreme-ip-lookup.com/json/')
+    const country = await fetch(
+      `https://extreme-ip-lookup.com/json/${getIP(req)}`
+    )
       .then((response) => response.json())
       .then((json) => json.countryCode)
 
-    return res.json({ country, ip: getIP(req) })
+    return res.json({ country })
   } catch (error) {
     return res.status(200).json(error)
   }
