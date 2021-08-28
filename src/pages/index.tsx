5c2b7a4a8b9aaf0cb067c0d89d39d5e79d4d7e1c
Daniel Esteves <estevesd8@gmail.com>
2021-08-27 23:42:34 -0400

    fix(css): size of the rings background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,12 @@ import type { Locale } from 'i18n'
 
 export const HomePage = (): JSX.Element => (
   <section className="container relative" id="danestves-section-hero">
-    <Rings className="mx-auto w-full h-auto max-w-[731px]" />
+    <Rings className="relative left-1/2 -translate-x-1/2 mx-auto w-[563px] h-auto sm:left-[46%] md:left-[38%] lg:left-auto lg:transform-none lg:w-full lg:max-w-[731px]" />
     <div className="absolute mt-2 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 md:mt-3">
       <LocalImage
         container={{
-          className: '-mt-16 h-auto max-w-[245px] md:mt-auto lg:max-w-[318px]',
+          className:
+            '-mt-16 h-auto w-[249px] sm:w-full sm:max-w-[248px] md:mt-auto lg:max-w-[320px]',
         }}
         image={{
           alt: '@danestves',
