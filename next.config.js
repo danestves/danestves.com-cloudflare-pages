307667aae4fe258aeea366cbf8a70610a509368b
Daniel Esteves <estevesd8@gmail.com>
2021-08-29 17:42:37 -0400

    feat: using proxy to avoid adblockers

--- next.config.js
+++ next.config.js
@@ -52,18 +52,6 @@ const nextConfig = {
       },
     ]
   },
-  async rewrites() {
-    return [
-      {
-        source: '/js/script.js',
-        destination: 'https://plausible.io/js/plausible.js',
-      },
-      {
-        source: '/api/event', // Or '/api/event/' if you have `trailingSlash: true` in this config
-        destination: 'https://plausible.io/api/event',
-      },
-    ]
-  },
 }
 
 module.exports = withPlugins([[withBundleAnalyzer], [withPreact]], nextConfig)
