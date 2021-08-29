e3c8195b0491313bd59ca7d20b617464ac1553a1
Daniel Esteves <estevesd8@gmail.com>
2021-08-29 01:53:02 -0400

    fix(lighthouse): removing dynamic imports from sections

--- src/components/Sections/index.ts
+++ src/components/Sections/index.ts
@@ -1,6 +1,3 @@
-// Dependencies
-import dynamic from 'next/dynamic'
-
-export const Hero = dynamic(() => import('./Hero'))
-export const LatestPosts = dynamic(() => import('./LatestPosts'))
-export const LatestVideos = dynamic(() => import('./LatestVideos'))
+export * from './Hero'
+export * from './LatestPosts'
+export * from './LatestVideos'
