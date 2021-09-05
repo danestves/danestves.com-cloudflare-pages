0c5566d63b68177874f07dd97ab8b50d939f9594
Daniel Esteves <estevesd8@gmail.com>
2021-09-05 15:26:11 -0400

    feat: responsive menu with popover

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,4 +1,6 @@
 // Dependencies
+import * as React from 'react'
+import { Popover, Transition } from '@headlessui/react'
 import { nanoid } from 'nanoid'
 import { useRouter } from 'next/router'
 import { usePlausible } from 'next-plausible'
@@ -9,14 +11,13 @@ import useWindowScroll from 'react-use/lib/useWindowScroll'
 import {
   LanguageSwitcher,
   Link,
-  LocalImage,
+  Logo,
   Search,
   ThemeSwitcher,
 } from '@/components'
-import { MenuIcon } from '@/components/Icons'
+import { MenuIcon, XIcon } from '@/components/Icons'
 import { MENU, SOCIAL } from '@/constants'
 import { clsx } from '@/utils'
-import AssetLogo from 'public/static/logo.png'
 import type { Locale } from 'i18n'
 
 export const Header = (): JSX.Element => {
@@ -34,14 +35,74 @@ export const Header = (): JSX.Element => {
         )}
       >
         <div className="flex items-center justify-between w-full">
-          <div className="flex items-center space-x-6">
-            <button
-              aria-label={t('components.layout.menu.toggle')}
-              className="block rounded-md focus:outline focus:ring-4 focus:ring-secondary focus:ring-opacity-50 focus:outline-none lg:hidden"
-              type="button"
-            >
-              <MenuIcon className="h-auto text-secondary w-[31px]" />
-            </button>
+          <div className="flex items-center space-x-6 lg:space-x-0">
+            <Popover className="lg:hidden">
+              <Popover.Button className="block rounded-md focus:outline focus:ring-4 focus:ring-secondary focus:ring-opacity-50 focus:outline-none">
+                <span className="sr-only">
+                  {t('components.layout.menu.toggle')}
+                </span>
+                <MenuIcon className="h-auto text-secondary w-[31px]" />
+              </Popover.Button>
+
+              <Transition
+                as={React.Fragment}
+                enter="duration-150 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Popover.Panel
+                  className="absolute inset-x-0 top-0 z-10 p-2 transition origin-top-left transform md:hidden"
+                  focus
+                >
+                  <div className="overflow-hidden bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5">
+                    <div className="flex items-center justify-between px-5 pt-4">
+                      <Link href="/" locale={router.locale}>
+                        <Logo className="w-auto h-8" />
+                      </Link>
+                      <div className="-mr-2">
+                        <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary">
+                          <span className="sr-only">
+                            {t('components.layout.menu.toggle')}
+                          </span>
+                          <XIcon aria-hidden="true" className="w-6 h-6" />
+                        </Popover.Button>
+                      </div>
+                    </div>
+                    <div className="px-2 pt-2 pb-3">
+                      {MENU.map((item) => (
+                        <Link
+                          className="block px-3 py-2 text-base text-[#989898] font-semibold rounded-md uppercase hover:text-primary"
+                          href={item.href}
+                          key={item.href}
+                          locale={router.locale}
+                        >
+                          {item.label[router.locale] || item.label}
+                        </Link>
+                      ))}
+                    </div>
+
+                    <div className="px-5 py-3 bg-gray-50">
+                      <div className="flex justify-between max-w-xs mx-auto">
+                        {SOCIAL.map((item) => (
+                          <Link
+                            className="block px-3 py-2 text-base text-[#989898] font-semibold rounded-md uppercase hover:text-primary"
+                            href={item.href}
+                            key={item.href}
+                            locale={router.locale}
+                          >
+                            <span className="sr-only">{item.label}</span>
+                            <item.icon aria-hidden="true" className="w-6 h-6" />
+                          </Link>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
 
             <div className="block lg:hidden">
               <ThemeSwitcher />
@@ -52,14 +113,8 @@ export const Header = (): JSX.Element => {
               href="/"
               locale={router.locale}
             >
-              <LocalImage
-                image={{
-                  alt: '@danestves',
-                  placeholder: 'blur',
-                  priority: true,
-                  src: AssetLogo,
-                }}
-              />
+              <span className="sr-only">@danestves</span>
+              <Logo aria-hidden="true" className="w-9 h-9" />
             </Link>
           </div>
 
@@ -69,17 +124,8 @@ export const Header = (): JSX.Element => {
               href="/"
               locale={router.locale}
             >
-              <LocalImage
-                container={{
-                  className: 'inline-flex',
-                }}
-                image={{
-                  alt: '@danestves',
-                  placeholder: 'blur',
-                  priority: true,
-                  src: AssetLogo,
-                }}
-              />
+              <span className="sr-only">@danestves</span>
+              <Logo aria-hidden="true" className="w-9 h-9" />
             </Link>
 
             {/* We put a padding left to simulate that in desktop is centered */}
--- src/constants/social.ts
+++ src/constants/social.ts
@@ -1,14 +1,20 @@
+// Internals
+import { GitHubIcon, TwitterIcon, YoutubeIcon } from '@/components/Icons'
+
 export const SOCIAL = [
   {
     href: '/twitter',
     label: 'twitter',
+    icon: TwitterIcon,
   },
   {
     href: '/youtube',
     label: 'youtube',
+    icon: YoutubeIcon,
   },
   {
     href: '/github',
     label: 'github',
+    icon: GitHubIcon,
   },
 ]
