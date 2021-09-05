b38d338f4ee5fb8cc4933727066622de14545dc1
Daniel Esteves <estevesd8@gmail.com>
2021-09-05 13:52:26 -0400

    feat: removing dynamic to load page more faster

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,23 +1,12 @@
-// Dependencies
-import dynamic from 'next/dynamic'
-
+export * from './Alert'
+export * from './ContentCard'
+export * from './Flag'
+export * from './GeneralObserver'
+export * from './LanguageSwitcher'
 export * from './Layout'
+export * from './Link'
+export * from './LocalImage'
+export * from './Logo'
+export * from './Search'
+export * from './ThemeSwitcher'
 export * from './Views'
-
-export const Alert = dynamic(() => import('./Alert').then((mod) => mod.Alert), {
-  ssr: false,
-})
-export const ContentCard = dynamic(() => import('./ContentCard'), {
-  ssr: false,
-})
-export const Flag = dynamic(() => import('./Flag'))
-export const GeneralObserver = dynamic(() => import('./GeneralObserver'), {
-  ssr: false,
-})
-export const LanguageSwitcher = dynamic(() => import('./LanguageSwitcher'))
-export const Link = dynamic(() => import('./Link'))
-export const LocalImage = dynamic(() => import('./LocalImage'))
-export const Logo = dynamic(() => import('./Logo/Logo'))
-export const Rings = dynamic(() => import('./Logo/Rings'))
-export const Search = dynamic(() => import('./Search'))
-export const ThemeSwitcher = dynamic(() => import('./ThemeSwitcher'))
