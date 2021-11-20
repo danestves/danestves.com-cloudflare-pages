c20b697224b6fc565231d1084e5443ca52e8d292
Daniel Esteves <estevesd8@gmail.com>
2021-11-20 17:19:59 -0400

    feat: reduce CLS removing framer motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 // Dependencies
 import * as React from 'react'
-import { LazyMotion, domAnimation, m } from 'framer-motion'
 import { I18nProvider } from 'next-rosetta'
 import { DefaultSeo, LogoJsonLd, SocialProfileJsonLd } from 'next-seo'
 import PlausibleProvider from 'next-plausible'
@@ -36,60 +35,40 @@ export const App: NextComponentType<
     >
       <PlausibleProvider domain="danestves.com">
         <I18nProvider table={pageProps.table}>
-          {getLayout(
-            <>
-              <LazyMotion features={domAnimation}>
-                <m.div
-                  animate="pageAnimate"
-                  initial="pageInitial"
-                  key={router.route}
-                  variants={{
-                    pageInitial: {
-                      opacity: 0,
-                    },
-                    pageAnimate: {
-                      opacity: 1,
-                    },
-                  }}
-                >
-                  <DefaultSeo
-                    {...defaultSeo(router.locale)}
-                    canonical={basePath}
-                    defaultOpenGraphImageHeight={630}
-                    defaultOpenGraphImageWidth={1200}
-                    openGraph={{
-                      ...defaultSeo(router.locale).openGraph,
-                      images: [
-                        {
-                          url: `https://cdn.flyyer.io/v2/danestves/_/_${lang}${router.asPath}`,
-                          alt: texts.title[router.locale],
-                          height: 630,
-                          width: 1200,
-                        },
-                      ],
-                      url: basePath,
-                    }}
-                  />
-                  <LogoJsonLd
-                    logo="https://danestves.com/static/logo.png"
-                    url="https://danestves.com"
-                  />
-                  <SocialProfileJsonLd
-                    name="Daniel Esteves"
-                    sameAs={[
-                      'https://twitter.com/danestves',
-                      'https://instagram.com/danestves',
-                      'https://www.linkedin.com/in/danestves',
-                      'https://www.youtube.com/channel/UC6YYVDKZC3mu1iB8IOCFqcw',
-                    ]}
-                    type="Person"
-                    url="https://danestves.com"
-                  />
-                  <Component {...pageProps} />
-                </m.div>
-              </LazyMotion>
-            </>
-          )}
+          <DefaultSeo
+            {...defaultSeo(router.locale)}
+            canonical={basePath}
+            defaultOpenGraphImageHeight={630}
+            defaultOpenGraphImageWidth={1200}
+            openGraph={{
+              ...defaultSeo(router.locale).openGraph,
+              images: [
+                {
+                  url: `https://cdn.flyyer.io/v2/danestves/_/_${lang}${router.asPath}`,
+                  alt: texts.title[router.locale],
+                  height: 630,
+                  width: 1200,
+                },
+              ],
+              url: basePath,
+            }}
+          />
+          <LogoJsonLd
+            logo="https://danestves.com/static/logo.png"
+            url="https://danestves.com"
+          />
+          <SocialProfileJsonLd
+            name="Daniel Esteves"
+            sameAs={[
+              'https://twitter.com/danestves',
+              'https://instagram.com/danestves',
+              'https://www.linkedin.com/in/danestves',
+              'https://www.youtube.com/channel/UC6YYVDKZC3mu1iB8IOCFqcw',
+            ]}
+            type="Person"
+            url="https://danestves.com"
+          />
+          {getLayout(<Component {...pageProps} />)}
         </I18nProvider>
       </PlausibleProvider>
     </ThemeProvider>
