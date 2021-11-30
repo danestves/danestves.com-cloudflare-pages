191ac0473898440b8e75331ef7d814a5311f5a22
Daniel Esteves <danestves@gmail.com>
2021-11-29 20:44:36 -0400

    fix: adding alias for preact

--- next.config.js
+++ next.config.js
@@ -70,6 +70,7 @@ const nextConfig = {
         react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
         'react-dom': 'preact/compat',
+        'react/jsx-runtime': 'preact/jsx-runtime',
       })
     }
 
