6dfad72d38c29967ad13bd390d91089d2fb86130
Daniel Esteves <danestves@gmail.com>
2022-01-17 03:30:41 -0400

    feat: adding logs for set theme to see the output

--- app/routes/action/set-theme.ts
+++ app/routes/action/set-theme.ts
@@ -1,12 +1,35 @@
 // Dependencies
-import { createThemeAction } from 'remix-themes';
+import { json } from 'remix';
+import { isTheme } from 'remix-themes';
 import { notFound } from 'remix-utils';
 import type { ActionFunction, LoaderFunction } from 'remix';
 
 // Internals
 import { themeSessionResolver } from '~/utils/theme.server';
 
-export let action: ActionFunction = createThemeAction(themeSessionResolver);
+export let action: ActionFunction = async ({ request }) => {
+  let session = await themeSessionResolver(request);
+  let { theme } = await request.json();
+
+  if (!isTheme(theme)) {
+    return json({
+      success: false,
+      message: `theme value of ${theme} is not a valid theme.`,
+    });
+  }
+
+  session.setTheme(theme);
+  return json(
+    {
+      success: true,
+      setTheme: session.setTheme(theme),
+      'Set-Cookie': await session.commit(),
+    },
+    {
+      headers: { 'Set-Cookie': await session.commit() },
+    }
+  );
+};
 
 export let loader: LoaderFunction = async () => {
   return notFound({
