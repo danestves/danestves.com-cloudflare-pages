d708b9734cbd511215b251a6878fd70b8aee8efd
Daniel Esteves <danestves@gmail.com>
2022-01-21 19:24:38 -0400

    fix: client only for switch

--- app/components/switch.tsx
+++ app/components/switch.tsx
@@ -1,8 +1,13 @@
 // Dependencis
 import * as SwitchPrimitive from '@radix-ui/react-switch';
+import { ClientOnly } from 'remix-utils';
 
 const Switch = (props: SwitchPrimitive.SwitchProps): JSX.Element => {
-  return <SwitchPrimitive.Root {...props} />;
+  return (
+    <ClientOnly>
+      <SwitchPrimitive.Root {...props} />
+    </ClientOnly>
+  );
 };
 
 export { Switch };
