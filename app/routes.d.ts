c84d755766809d1e084b746dff1df2f237c8c89e
Daniel Esteves <danestves@gmail.com>
2022-01-23 20:25:39 -0400

    feat: how-typescript-enforced-cleaner-architecture-in-react-app

--- app/routes.d.ts
+++ app/routes.d.ts
@@ -1,22 +1,22 @@
-declare module 'routes-gen' {
+declare module "routes-gen" {
   export type RouteParams = {
-    '/action/update-content-sha': {};
-    '/action/post-content': {};
-    '/api/get-content-sha': {};
-    '/action/set-theme': {};
-    '/posts/:slug': { slug: string };
-    '/': {};
-    '/posts': {};
+    "/action/update-content-sha": {};
+    "/action/post-content": {};
+    "/api/get-content-sha": {};
+    "/action/set-theme": {};
+    "/posts/:slug": { slug: string };
+    "/": {};
+    "/posts": {};
   };
 
   export function route<
     T extends
-      | ['/action/update-content-sha']
-      | ['/action/post-content']
-      | ['/api/get-content-sha']
-      | ['/action/set-theme']
-      | ['/posts/:slug', RouteParams['/posts/:slug']]
-      | ['/']
-      | ['/posts']
+      | ["/action/update-content-sha"]
+      | ["/action/post-content"]
+      | ["/api/get-content-sha"]
+      | ["/action/set-theme"]
+      | ["/posts/:slug", RouteParams["/posts/:slug"]]
+      | ["/"]
+      | ["/posts"]
   >(...args: T): typeof args[0];
 }
