4c7edda2cdb1392f9f174542d76a89a6e48eaf58
Daniel Esteves <me+github@danestves.com>
2022-02-05 16:22:23 -0400

    feat: add sandpack to render code

--- app/components/mdx-components/index.tsx
+++ app/components/mdx-components/index.tsx
@@ -1,12 +1,29 @@
 // Dependencies
+import {
+  Sandpack,
+  defaultDark,
+  defaultLight,
+} from '@codesandbox/sandpack-react';
 import { CodePen, CodeSandbox } from 'mdx-embed';
+import type { SandpackProps } from '@codesandbox/sandpack-react';
 
 // Internals
 import { Pre } from './pre';
+import { Theme, useTheme } from 'remix-themes';
 
 let mdxComponents = {
   CodePen,
   CodeSandbox,
+  Sandpack: (props: SandpackProps) => {
+    let [theme] = useTheme();
+
+    return (
+      <Sandpack
+        {...props}
+        theme={theme === Theme.DARK ? defaultDark : defaultLight}
+      />
+    );
+  },
   pre: Pre,
 };
 
--- other/mdx/mdx-components.mjs
+++ other/mdx/mdx-components.mjs
@@ -1,9 +1,11 @@
 // Dependencies
+import CS from '@codesandbox/sandpack-react';
 import { CodePen, CodeSandbox } from 'mdx-embed';
 
 let mdxComponents = {
   CodePen,
   CodeSandbox,
+  Sandpack: CS.Sandpack,
 };
 
 export { mdxComponents };
