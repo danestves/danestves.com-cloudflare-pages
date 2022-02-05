266fe3f11a3940dbf989962c3d4565598b353281
Daniel Esteves <me+github@danestves.com>
2022-02-05 19:46:00 -0400

    feat: rever to old codesandbox

--- app/components/mdx-components/index.tsx
+++ app/components/mdx-components/index.tsx
@@ -1,29 +1,12 @@
 // Dependencies
-import {
-  Sandpack,
-  defaultDark,
-  defaultLight,
-} from '@codesandbox/sandpack-react';
 import { CodePen, CodeSandbox } from 'mdx-embed';
-import type { SandpackProps } from '@codesandbox/sandpack-react';
 
 // Internals
 import { Pre } from './pre';
-import { Theme, useTheme } from 'remix-themes';
 
 let mdxComponents = {
   CodePen,
   CodeSandbox,
-  Sandpack: (props: SandpackProps) => {
-    let [theme] = useTheme();
-
-    return (
-      <Sandpack
-        {...props}
-        theme={theme === Theme.DARK ? defaultDark : defaultLight}
-      />
-    );
-  },
   pre: Pre,
 };
 
--- other/mdx/mdx-components.mjs
+++ other/mdx/mdx-components.mjs
@@ -1,11 +1,9 @@
 // Dependencies
-import CS from '@codesandbox/sandpack-react';
 import { CodePen, CodeSandbox } from 'mdx-embed';
 
 let mdxComponents = {
   CodePen,
   CodeSandbox,
-  Sandpack: CS.Sandpack,
 };
 
 export { mdxComponents };
