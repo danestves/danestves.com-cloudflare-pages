1d132e454ac83484c1df9a4cd4252f8c92303507
Daniel Esteves <me+github@danestves.com>
2022-02-16 17:30:31 -0400

    fix: correct output for remix

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,21 +1,21 @@
 // Dependencies
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
+import * as build from '@remix-run/dev/server-build';
 
 // Internals
 import { createContext } from './context';
-import * as build from '../build';
 
 const handleRequest = createPagesFunctionHandler({
-  // @ts-ignore
   build,
   getLoadContext: ({ env, request, ...ctx }) => {
     return {
       env,
+      // @ts-ignore: context is well defined
       ...createContext(request, env, ctx),
     };
   },
 });
 
-export function onRequest(context) {
+export function onRequest(context: EventContext<any, any, any>) {
   return handleRequest(context);
 }
--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,7 @@ module.exports = {
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
-  serverBuildPath: 'build',
+  serverBuildPath: 'functions/[[path]].js',
   serverBuildTarget: 'cloudflare-pages',
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ['.*'],
