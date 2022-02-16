d1bd31082811c3624e7e29310e7c6f00a1e26932
Daniel Esteves <me+github@danestves.com>
2022-02-16 18:18:36 -0400

    feat: minimal version of remix config

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,7 @@
 /**
- * @type {import('@remix-run/dev/config').AppConfig}
+ * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
   server: 'functions/[[path]].ts',
   serverBuildTarget: 'cloudflare-pages',
   devServerBroadcastDelay: 1000,
