ebdcc2c49489b24000d20d4471183d8006103f2f
Daniel Esteves <me+github@danestves.com>
2022-02-26 13:34:07 -0400

    feat: removing animation for hero

--- app/components/sections/hero-section.tsx
+++ app/components/sections/hero-section.tsx
@@ -1,5 +1,4 @@
-// Dependencies
-import { m as motion, useReducedMotion } from 'framer-motion';
+// Dependencies\
 import { useTranslation } from 'react-i18next';
 
 // Internals
@@ -7,10 +6,7 @@ import { getImageBuilder, getImgProps, images } from '~/images';
 import { BlurrableImage } from '../blurrable-image';
 import { Rings } from '../rings';
 
-let MotionRings = motion(Rings);
-
 function HeroSection() {
-  let shouldReduceMotion = useReducedMotion();
   let { t } = useTranslation('sections');
 
   return (
@@ -19,59 +15,37 @@ function HeroSection() {
       id="danestves-section-hero"
     >
       <div className="relative left-1/2 mx-auto h-auto w-[563px] -translate-x-1/2 sm:left-[46%] md:left-[38%] lg:left-auto lg:w-full lg:max-w-[731px] lg:transform-none">
-        <MotionRings
-          animate={{ opacity: 1, scale: 1 }}
-          className="h-auto w-full"
-          initial={{ opacity: 0, scale: shouldReduceMotion ? 1 : 0.75 }}
-          transition={{ duration: 0.5 }}
-        />
+        <Rings className="h-auto w-full" />
       </div>
 
       <div className="absolute top-1/2 left-1/2 mt-2 -translate-x-1/2 -translate-y-1/2 lg:mt-3">
         <div className="-mt-16 h-auto w-[249px] max-w-none sm:w-[248px] md:mt-auto lg:w-[320px]">
-          <motion.div
-            animate={{ opacity: 1, scale: 1 }}
-            initial={{ opacity: 0, scale: shouldReduceMotion ? 1 : 0.75 }}
-            transition={{ duration: 0.5 }}
-          >
-            <BlurrableImage
-              className="relative pb-[calc((637/646)*100%)]"
-              img={
-                <img
-                  className="absolute w-full"
-                  {...getImgProps(
-                    getImageBuilder(images.heroMask.id, images.heroMask.alt),
-                    {
-                      widths: [318, 646, 984, 1312, 1740],
-                      sizes: [
-                        '(max-width: 1023px) 80vw',
-                        '(min-width: 1024px) and (max-width: 1279px) 50vw',
-                        '(min-width: 1280px) 984px',
-                      ],
-                    }
-                  )}
-                  height={646}
-                  width={637}
-                />
-              }
-            />
-          </motion.div>
+          <BlurrableImage
+            className="relative pb-[calc((637/646)*100%)]"
+            img={
+              <img
+                className="absolute w-full"
+                {...getImgProps(
+                  getImageBuilder(images.heroMask.id, images.heroMask.alt),
+                  {
+                    widths: [318, 646, 984, 1312, 1740],
+                    sizes: [
+                      '(max-width: 1023px) 80vw',
+                      '(min-width: 1024px) and (max-width: 1279px) 50vw',
+                      '(min-width: 1280px) 984px',
+                    ],
+                  }
+                )}
+                height={646}
+                width={637}
+              />
+            }
+          />
         </div>
       </div>
 
       {/* Floating Card */}
-      <motion.div
-        animate={{ opacity: 1, scale: 1 }}
-        className="mx-auto -mt-16 w-full max-w-[332px] rounded-[20px] bg-secondary/80 pt-3 pr-5 pb-4 pl-8 backdrop-blur-sm dark:bg-secondary/[0.65] md:-mt-32 lg:absolute lg:top-1/2 lg:right-1/2 lg:mt-auto lg:mr-32"
-        initial={{
-          opacity: 0,
-          originX: 1,
-          originZ: 1,
-          scale: shouldReduceMotion ? 1 : 0.75,
-          translateY: -50,
-        }}
-        transition={{ duration: 0.5 }}
-      >
+      <div className="mx-auto -mt-16 w-full max-w-[332px] rounded-[20px] bg-secondary/80 pt-3 pr-5 pb-4 pl-8 backdrop-blur-sm dark:bg-secondary/[0.65] md:-mt-32 lg:absolute lg:top-1/2 lg:right-1/2 lg:mt-auto lg:mr-32">
         <h1 className="text-2xl font-black uppercase text-white">
           daniel esteves{' '}
           <span aria-label="victory hand" role="img">
@@ -94,7 +68,7 @@ function HeroSection() {
             }}
           />
         </p>
-      </motion.div>
+      </div>
 
       {/* Search Trigger */}
       <div className="absolute bottom-1/4 hidden lg:right-[13%] lg:block xl:right-[20%]">
